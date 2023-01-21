4ada88a72f48f30cbbfd4ccc1e50d7757e01c634
Deepak <deepakpunyani@ymail.com>
2023-01-21 15:41:36 +0530

    Functional components with rmga6 and export cnst

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+//Import constant
+import {Header} from './components/Header';
 
 
 //JSX
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-function Header(){
+// function Header(){
+//     return <h1>Fuctinal Components</h1>
+// }
+
+export const Header=() =>{
     return <h1>Fuctinal Components</h1>
+
 }
-
-
-export default Header;
+    
   
