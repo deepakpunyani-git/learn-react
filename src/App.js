664fa67166231b967a5c355df3a7e3483e945825
Deepak <deepakpunyani@ymail.com>
2023-01-21 16:47:48 +0530

    Class component

--- src/App.js
+++ src/App.js
@@ -2,47 +2,15 @@ import React from 'react';
 import './App.css';
 //Import constant
 import {Header} from './components/Header';
-
-
-//JSX
-//let name =<span> React JS</span>;
-
-//Common 
-//let newElement = React.createElement('h2',{className:"demo"},"This is demo text")
-
-// let demoFun = function(){
-//   alert("text")
-// }
-
-// let passFun = (marks) => {
-
-//   if(marks > 50){
-//     return "You are Pass";
-//   }else{
-//     return "You are Fail";
-
-//   }
-
-// } 
-
-// function App() {
-//   return (
-//     <div className="App">
-//         <p>
-//          Learn {name}
-//         </p>
-//         {newElement}
-//         <button click={demoFun}>Click ME</button><br></br>
-//         {passFun(10)}
-
-//     </div>
-//   );
-// }
+import CHeaderNew from './components/CHeader';
 
 class App extends React.Component{
   render(){
     return(
-      <Header/>
+      <div className='Main'>
+          <Header/>
+          <CHeaderNew/>
+      </div>
     );
   }
 }
---
+++ src/components/CHeader.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+class CHeader extends React.Component{
+    render(){
+      return <h1>Hello classA Component</h1>
+    }
+}
+
+export default CHeader
